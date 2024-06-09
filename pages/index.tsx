93436905573eedb2acd2bdee6afafd84ed4793c5
Animesh <animeshraj958@gmail.com>
2024-06-09 11:23:58 +0530

    footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,15 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Footer />
+    </>
   );
 }

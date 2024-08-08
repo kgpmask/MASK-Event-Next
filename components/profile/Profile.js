abf3c1973c1643ed1324f9c329cd0a1761ecd929
Animesh <animeshraj958@gmail.com>
2024-08-08 19:52:42 +0530

    cross icon

--- components/profile/Profile.js
+++ components/profile/Profile.js
@@ -2,15 +2,23 @@ import React from 'react'
 import Styles from '@/styles/Profile.module.css'
 // import Image from @next/image
 import next from 'next'
+import { IoClose } from "react-icons/io5";
+
 
 function Profile() {
     return (
         <div className={Styles['container']}>
             <div className={Styles["wrapper"]}>
-                <div style={{borderRadius:'10px'}}><div className={Styles["block"]}><img src='/deku.webp' alt='MASK' ></img></div><img src="/deku.webp" alt="MASK" className={Styles['profile-img']} /></div>
+                <div style={{ borderRadius: '10px' }}>
+                    <div className={Styles["block"]}>
+                        <IoClose className={Styles["cross"]} color='white' />
+                        <img src='/deku.webp' alt='MASK' />
+                    </div>
+                    <img src="/deku.webp" alt="MASK" className={Styles['profile-img']} />
+                </div>
                 <div className={Styles["bottom-content"]}></div>
             </div>
-        </div>
+        </div >
     )
 }
 

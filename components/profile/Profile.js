ee4594f7942dea1ce40855483a184615f313ead6
Animesh <animeshraj958@gmail.com>
2024-08-08 20:02:35 +0530

    userinfo

--- components/profile/Profile.js
+++ components/profile/Profile.js
@@ -6,17 +6,25 @@ import { IoClose } from "react-icons/io5";
 
 
 function Profile() {
+    const handleClose = () => {
+        console.log('close')
+    }
     return (
         <div className={Styles['container']}>
             <div className={Styles["wrapper"]}>
                 <div style={{ borderRadius: '10px' }}>
                     <div className={Styles["block"]}>
-                        <IoClose className={Styles["cross"]} color='white' />
+                        <IoClose className={Styles["cross"]} color='white' onClick={handleClose} />
                         <img src='/deku.webp' alt='MASK' />
                     </div>
                     <img src="/deku.webp" alt="MASK" className={Styles['profile-img']} />
                 </div>
-                <div className={Styles["bottom-content"]}></div>
+                <div className={Styles["bottom-content"]}>
+                    <div className={Styles["profile-info"]}>
+                        <h1>Profile</h1>
+                        <p><span>deku</span></p>
+                    </div>
+                </div>
             </div>
         </div >
     )

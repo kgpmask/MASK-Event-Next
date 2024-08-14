62ab5c91de89aa00f277d5a5fc658e2c7c4cf036
Ankan <93087057+ItsAnkan@users.noreply.github.com>
2024-08-14 22:42:52 +0530

    portttttttt

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const dbInit = require("./database/dbInit.js");
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = 3000;
+const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();

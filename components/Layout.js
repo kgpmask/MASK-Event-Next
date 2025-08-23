fd30248da284b3636237cd2894f2a7e86fbcad8a
Jx-ls <joshuarajtadi@gmail.com>
2025-08-23 06:50:13 +0530

    fix: space-between in live page

--- components/Layout.js
+++ components/Layout.js
@@ -5,19 +5,35 @@ import { useRouter } from "next/router";
 
 export default function Home({ children }) {
 	const router = useRouter();
-	const hideFooter = router.pathname.startsWith("/live");
-	return (
-		<>
-			<div className={Styles["background"]}></div>
-			<div className={Styles["flex-container"]}>
-				<Navbar />
-				<main className={Styles["container"]}>
-					<div id="content-wrap">
-						{children}
-					</div>
-				</main>
-				{!hideFooter && <Footer />}
-			</div>
-		</>
-	);
+	const live = router.pathname.startsWith("/live");
+	if (live) {
+		return (
+			<>
+				<div className={Styles["background"]}></div>
+				<div className={Styles["flex-container-live"]}>
+					<Navbar />
+					<main className={Styles["container"]}>
+						<div id="content-wrap">
+							{children}
+						</div>
+					</main>
+				</div>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<div className={Styles["background"]}></div>
+				<div className={Styles["flex-container"]}>
+					<Navbar />
+					<main className={Styles["container"]}>
+						<div id="content-wrap">
+							{children}
+						</div>
+					</main>
+					<Footer />
+				</div>
+			</>
+		);
+	}
 }
